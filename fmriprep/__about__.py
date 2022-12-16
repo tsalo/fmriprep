1ae319369a7e3ff0d4f0daea38849758d1006b2d
Chris Markiewicz <effigies@gmail.com>
2022-12-16 14:20:56 -0500

    MNT: Switch from versioneer to setuptools_scm
    We can drop MANIFEST.in and custom package data declarations by letting
    setuptools_scm package everything version-controlled in the sdist.

--- fmriprep/__about__.py
+++ fmriprep/__about__.py
@@ -21,10 +21,11 @@
 #     https://www.nipreps.org/community/licensing/
 #
 """Base module variables."""
-from ._version import get_versions
 
-__version__ = get_versions()['version']
-del get_versions
+try:
+    from ._version import __version__
+except ImportError:
+    __version__ == "0+unknown"
 
 __packagename__ = 'fmriprep'
 __copyright__ = 'Copyright 2022, The NiPreps Developers'
