7ab02a0e617abcc6f1fc915a36a573cda50553a6
celprov <celine.provins@unil.ch>
2023-12-13 13:45:33 -0800

    fix: delete Report patch subclass as it is not useful anymore

--- fmriprep/reports/core.py
+++ fmriprep/reports/core.py
@@ -22,34 +22,7 @@
 #
 from pathlib import Path
 
-from nireports.assembler.report import Report as _Report
-
-# This patch is intended to permit fMRIPrep 20.2.0 LTS to use the YODA-style
-# derivatives directory. Ideally, we will remove this in 20.3.x and use an
-# updated niworkflows.
-
-
-class Report(_Report):
-    def _load_config(self, config):
-        from yaml import safe_load as load
-
-        settings = load(config.read_text())
-
-        # Removed from here: Appending self.packagename to self.root and self.out_dir
-        # In this version, pass reportlets_dir and out_dir with fmriprep in the path.
-
-        if self.subject_id is not None:
-            self.root = self.root / f"sub-{self.subject_id}"
-
-        if "template_path" in settings:
-            self.template_path = config.parent / settings["template_path"]
-
-        self.index(settings["sections"])
-
-
-#
-# The following are the interface used directly by fMRIPrep
-#
+from nireports.assembler.report import Report
 
 
 def generate_reports(subject_list, output_dir, run_uuid, config=None, work_dir=None):
