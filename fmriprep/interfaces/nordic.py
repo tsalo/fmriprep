a21d415d88232dbbd4a0ed3ada4373be45f87b25
Taylor Salo <tsalo006@fiu.edu>
2024-08-08 15:39:14 -0400

    Update nordic.py

--- fmriprep/interfaces/nordic.py
+++ fmriprep/interfaces/nordic.py
@@ -1,8 +1,11 @@
-"""BIDS-related interfaces."""
+"""NORDIC-related interfaces."""
 
+import os
 from pathlib import Path
+from string import Template
 
 from nipype.interfaces.base import (
+    BaseInterface,
     BaseInterfaceInputSpec,
     File,
     SimpleInterface,
@@ -10,6 +13,7 @@ from nipype.interfaces.base import (
     isdefined,
     traits,
 )
+from nipype.interfaces.matlab import MatlabCommand
 
 
 class _ValidateComplexInputSpec(BaseInterfaceInputSpec):
@@ -164,3 +168,91 @@ class RemoveNoise(SimpleInterface):
         self._results['bold_file'] = str(out_file)
 
         return runtime
+
+
+class _NORDICInputSpec(BaseInterfaceInputSpec):
+    mag_file = File(exists=True, mandatory=True)
+    phase_file = File(exists=True, mandatory=False)
+    n_noise_volumes = traits.Int(mandatory=False, default=0, usedefault=True)
+    out_prefix = traits.Str('denoised', usedefault=True)
+
+
+class _NORDICOutputSpec(TraitedSpec):
+    mag_file = File(exists=True)
+    phase_file = File(exists=True)
+
+
+class NORDIC(BaseInterface):
+    input_spec = _NORDICInputSpec
+    output_spec = _NORDICOutputSpec
+
+    def _run_interface(self, runtime):
+        d = {
+            'mag_file': self.inputs.mag_file,
+            'out_dir': os.getcwd(),
+            'out_prefix': self.inputs.out_prefix,
+            'n_noise_vols': self.inputs.n_noise_vols,
+        }
+        if isdefined(self.inputs.phase_file):
+            d['phase_file'] = self.inputs.phase_file
+            d['magnitude_only'] = 0
+        else:
+            d['phase_file'] = ''
+            d['magnitude_only'] = 1
+
+        # This is your MATLAB code template
+        script = Template(
+            """
+% A template MATLAB script to run NORDIC on a magnitude+phase file pair.
+% Settings come from Thomas Madison.
+
+% Set args as recommended for fMRI
+% Set to 0 if input includes both magnitude + phase timeseries
+ARG.magnitude_only = $magnitude_only;
+% Save out the phase data too
+ARG.make_complex_nii = 1;
+% Set to 1 for fMRI
+ARG.temporal_phase = 1;
+% Set to 1 to enable NORDIC denoising
+ARG.NORDIC = 1;
+% Use 10 for fMRI
+ARG.phase_filter_width = 10;
+% Set equal to number of noise frames at end of scan, if present
+ARG.noise_volume_last = $n_noise_vols;
+% DIROUT may need to be separate from fn_out
+ARG.DIROUT = '$out_dir';
+
+fn_magn_in = '$mag_file';
+fn_phase_in = '$phase_file';
+fn_out = '$out_prefix'
+
+% Add the NORDIC code
+addpath('/path/to/nifti/library/')
+addpath('/path/to/NORDIC_Raw/')
+
+% Call NORDIC on the input files
+NIFTI_NORDIC(fn_magn_in, fn_phase_in, fn_out, ARG)
+exit;
+"""
+        ).substitute(d)
+
+        # mfile = True  will create an .m file with your script and executed.
+        # Alternatively
+        # mfile can be set to False which will cause the matlab code to be
+        # passed
+        # as a commandline argument to the matlab executable
+        # (without creating any files).
+        # This, however, is less reliable and harder to debug
+        # (code will be reduced to
+        # a single line and stripped of any comments).
+        mlab = MatlabCommand(script=script, mfile=True)
+        result = mlab.run()
+        return result.runtime
+
+    def _list_outputs(self):
+        outputs = self._outputs().get()
+        prefix = self.inputs.out_prefix
+        outputs['mag_file'] = os.path.join(os.getcwd(), f'{prefix}magn.nii')
+        if isdefined(self.inputs.phase_file):
+            outputs['phase_file'] = os.path.join(os.getcwd(), f'{prefix}phase.nii')
+        return outputs
