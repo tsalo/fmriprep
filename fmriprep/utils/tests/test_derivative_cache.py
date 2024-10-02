565d1a97b40d0cea0422850c4a17c7df908ba6c9
Patrick Sadil <psadil@gmail.com>
2024-10-02 11:00:23 -0400

    FIX: Repair and test query for precalculated baseline/boldref files (#3370)

--- fmriprep/utils/tests/test_derivative_cache.py
+++ fmriprep/utils/tests/test_derivative_cache.py
@@ -5,6 +5,28 @@ import pytest
 from fmriprep.utils import bids
 
 
+@pytest.mark.parametrize('desc', ['hmc', 'coreg'])
+def test_baseline_found_as_str(tmp_path: Path, desc: str):
+    subject = '0'
+    task = 'rest'
+
+    to_find = tmp_path.joinpath(
+        f'sub-{subject}', 'func', f'sub-{subject}_task-{task}_desc-{desc}_boldref.nii.gz'
+    )
+    to_find.parent.mkdir(parents=True)
+    to_find.touch()
+
+    entities = {
+        'subject': subject,
+        'task': task,
+        'suffix': 'bold',
+        'extension': '.nii.gz',
+    }
+
+    derivs = bids.collect_derivatives(derivatives_dir=tmp_path, entities=entities)
+    assert dict(derivs) == {f'{desc}_boldref': str(to_find), 'transforms': {}}
+
+
 @pytest.mark.parametrize('xfm', ['boldref2fmap', 'boldref2anat', 'hmc'])
 def test_transforms_found_as_str(tmp_path: Path, xfm: str):
     subject = '0'
