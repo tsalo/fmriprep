1144dde5d7e7e90a42fe41d50f86af3bab6c0c8e
Christopher J. Markiewicz <markiewicz@stanford.edu>
2025-03-25 14:59:31 -0400

    chore: Add inline dependencies for update_zenodo.py

--- .maint/update_zenodo.py
+++ .maint/update_zenodo.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
+# /// script
+# requires-python = ">=3.12"
+# dependencies = [
+#     "fuzzywuzzy",
+#     "python-levenshtein",
+# ]
+# ///
 """Update and sort the creators list of the zenodo record."""
 
 import json
