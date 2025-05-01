4ce64c0e584387deb2d5f14e80edd056310682ce
Ma Feilong <mafeilong@gmail.com>
2025-05-01 15:31:23 -0400

    Add init_bold_surf_wb_wf, which resamples data to surfaces using connectome workbench.

--- fmriprep/workflows/bold/resampling.py
+++ fmriprep/workflows/bold/resampling.py
@@ -514,6 +514,212 @@ def init_goodvoxels_bold_mask_wf(mem_gb: float, name: str = 'goodvoxels_bold_mas
     return workflow
 
 
+def init_bold_surf_wb_wf(
+    space: str,
+    density: ty.Literal['10k', '32k', '41k'],
+    omp_nthreads: int,
+    mem_gb: float,
+    name: str = 'bold_surf_wb_wf',
+):
+    """Resample BOLD time series to surface using the Connectome Workbench.
+
+    This workflow is modified from ``init_bold_fsLR_resampling_wf``.
+
+    Workflow Graph
+        .. workflow::
+            :graph2use: colored
+            :simple_form: yes
+
+            from fmriprep.workflows.bold.resampling import init_bold_surf_wb_wf
+            wf = init_bold_surf_wb_wf(
+                space='onavg',
+                density='10k',
+                omp_nthreads=1,
+                mem_gb=1,
+            )
+    Parameters
+    ----------
+    space : :class:`str`
+        Surface template space, such as ``"onavg"`` or ``"fsLR"``.
+    density : :class:`str`
+        Either ``"10k"``, ``"32k"``, or ``"41k"``, representing the number of vertices
+        per hemisphere.
+    omp_nthreads : :class:`int`
+        Maximum number of threads an individual process may use.
+    mem_gb : :class:`float`
+        Size of BOLD file in GB.
+    name : :class:`str`
+        Name of workflow (default: ``bold_surf_wb_wf``).
+
+    Inputs
+    ------
+    bold_file : :class:`str`
+        Path to BOLD file resampled into T1 space
+    white : :class:`list` of :class:`str`
+        Path to left and right hemisphere white matter GIFTI surfaces.
+    pial : :class:`list` of :class:`str`
+        Path to left and right hemisphere pial GIFTI surfaces.
+    midthickness : :class:`list` of :class:`str`
+        Path to left and right hemisphere midthickness GIFTI surfaces.
+    midthickness_resampled : :class:`list` of :class:`str`
+        Path to left and right hemisphere midthickness GIFTI surfaces resampled into the output
+        space.
+    sphere_reg_fsLR : :class:`list` of :class:`str`
+        Path to left and right hemisphere sphere.reg GIFTI surfaces, mapping from subject to fsLR.
+    volume_roi : :class:`str` or Undefined
+        Pre-calculated goodvoxels mask. Not required.
+
+    Outputs
+    -------
+    bold_resampled : :class:`list` of :class:`str`
+        Path to BOLD series resampled as functional GIFTI files in template space.
+
+    """
+    import templateflow.api as tf
+    from niworkflows.engine.workflows import LiterateWorkflow as Workflow
+    from niworkflows.interfaces.utility import KeySelect
+
+    from fmriprep.interfaces.workbench import VolumeToSurfaceMapping
+
+    workflow = Workflow(name=name)
+
+    space_str = 'onavg [@onavg]' if space == 'onavg' else space
+    workflow.__desc__ = f"""\
+The BOLD time-series were resampled onto the {space_str} space
+using the Connectome Workbench [@hcppipelines].
+"""
+
+    inputnode = pe.Node(
+        niu.IdentityInterface(
+            fields=[
+                'bold_file',
+                'white',
+                'pial',
+                'midthickness',
+                'midthickness_resampled',
+                'sphere_reg_fsLR',
+                # 'cortex_mask',
+                'volume_roi',
+            ]
+        ),
+        name='inputnode',
+    )
+
+    hemisource = pe.Node(
+        niu.IdentityInterface(fields=['hemi']),
+        name='hemisource',
+        iterables=[('hemi', ['L', 'R'])],
+    )
+
+    joinnode = pe.JoinNode(
+        niu.IdentityInterface(fields=['bold_resampled']),
+        name='joinnode',
+        joinsource='hemisource',
+    )
+
+    outputnode = pe.Node(
+        niu.IdentityInterface(fields=['bold_resampled']),
+        name='outputnode',
+    )
+
+    # select white, midthickness and pial surfaces based on hemi
+    select_surfaces = pe.Node(
+        KeySelect(
+            fields=[
+                'white',
+                'pial',
+                'midthickness',
+                'midthickness_resampled',
+                'sphere_reg_fsLR',
+                'template_sphere',
+                # 'cortex_mask',
+                # 'template_roi',
+            ],
+            keys=['L', 'R'],
+        ),
+        name='select_surfaces',
+        run_without_submitting=True,
+    )
+    select_surfaces.inputs.template_sphere = [
+        str(sphere)
+        for sphere in tf.get(
+            template=space,
+            space=('fsLR' if space != 'fsLR' else None),
+            density=density,
+            suffix='sphere',
+            extension='.surf.gii',
+        )
+    ]
+
+    # RibbonVolumeToSurfaceMapping.sh
+    # Line 85 thru ...
+    volume_to_surface = pe.Node(
+        VolumeToSurfaceMapping(method='ribbon-constrained'),
+        name='volume_to_surface',
+        mem_gb=mem_gb * 3,
+        n_procs=omp_nthreads,
+    )
+    metric_dilate = pe.Node(
+        MetricDilate(distance=10, nearest=True),
+        name='metric_dilate',
+        mem_gb=1,
+        n_procs=omp_nthreads,
+    )
+    # mask_native = pe.Node(MetricMask(), name='mask_native')
+    resample_to_template = pe.Node(
+        MetricResample(method='ADAP_BARY_AREA', area_surfs=True),
+        name='resample_to_template',
+        mem_gb=1,
+        n_procs=omp_nthreads,
+    )
+    # ... line 89
+    # mask_fsLR = pe.Node(MetricMask(), name='mask_fsLR')
+
+    workflow.connect([
+        (inputnode, select_surfaces, [
+            ('white', 'white'),
+            ('pial', 'pial'),
+            ('midthickness', 'midthickness'),
+            ('midthickness_resampled', 'midthickness_resampled'),
+            ('sphere_reg_fsLR', 'sphere_reg_fsLR'),
+            # ('cortex_mask', 'cortex_mask'),
+        ]),
+        (hemisource, select_surfaces, [('hemi', 'key')]),
+        # Resample BOLD to native surface, dilate and mask
+        (inputnode, volume_to_surface, [
+            ('bold_file', 'volume_file'),
+            ('volume_roi', 'volume_roi'),
+        ]),
+        (select_surfaces, volume_to_surface, [
+            ('midthickness', 'surface_file'),
+            ('white', 'inner_surface'),
+            ('pial', 'outer_surface'),
+        ]),
+        (select_surfaces, metric_dilate, [('midthickness', 'surf_file')]),
+        # (select_surfaces, mask_native, [('cortex_mask', 'mask')]),
+        (volume_to_surface, metric_dilate, [('out_file', 'in_file')]),
+        # (metric_dilate, mask_native, [('out_file', 'in_file')]),
+        # Resample BOLD to fsLR and mask
+        (select_surfaces, resample_to_template, [
+            ('sphere_reg_fsLR', 'current_sphere'),
+            ('template_sphere', 'new_sphere'),
+            ('midthickness', 'current_area'),
+            ('midthickness_resampled', 'new_area'),
+            # ('cortex_mask', 'roi_metric'),
+        ]),
+        (metric_dilate, resample_to_template, [('out_file', 'in_file')]),
+        # (mask_native, resample_to_template, [('out_file', 'in_file')]),
+        # (select_surfaces, mask_fsLR, [('template_roi', 'mask')]),
+        # (resample_to_fsLR, mask_fsLR, [('out_file', 'in_file')]),
+        # Output
+        # (mask_fsLR, joinnode, [('out_file', 'bold_fsLR')]),
+        (resample_to_template, joinnode, [('out_file', 'bold_resampled')]),
+        (joinnode, outputnode, [('bold_resampled', 'bold_resampled')]),
+    ])  # fmt:skip
+
+    return workflow
+
+
 def init_bold_fsLR_resampling_wf(
     grayord_density: ty.Literal['91k', '170k'],
     omp_nthreads: int,
