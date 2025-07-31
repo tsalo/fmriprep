fc77f48c3498b7c0e5522228d0bd93f655cfd203
Taylor Salo <tsalo90@gmail.com>
2025-07-31 13:21:14 -0400

    Write out cortex mask giftis.

--- fmriprep/workflows/base.py
+++ fmriprep/workflows/base.py
@@ -473,6 +473,18 @@ It is released under the [CC0]\
                 },
                 name='ds_fsLR_surfaces_wf',
             )
+            ds_cortex_mask = pe.MapNode(
+                DerivativesDataSink(
+                    base_directory=config.execution.fmriprep_dir,
+                    datatype='anat',
+                    desc='cortex',
+                    suffix='mask',
+                    dismiss_entities=dismiss_echo(),
+                ),
+                iterfield=['in_file', 'hemisphere'],
+                name='ds_cortex_mask',
+            )
+            ds_cortex_mask.inputs.hemisphere = ['L', 'R']
 
             workflow.connect([
                 (anat_fit_wf, curv_wf, [
@@ -504,6 +516,7 @@ It is released under the [CC0]\
                         'inputnode.sphere_reg_fsLR',
                     ),
                 ]),
+                (hcp_morphometrics_wf, ds_cortex_mask, [('outputnode.roi', 'in_file')]),
                 (hcp_morphometrics_wf, morph_grayords_wf, [
                     ('outputnode.curv', 'inputnode.curv'),
                     ('outputnode.thickness', 'inputnode.thickness'),
