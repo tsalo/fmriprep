a7d0aa68fe43a260cb382bafd334dccd0fd8c4c4
Jon Haitz Legarreta Gorroño <jon.haitz.legarreta@gmail.com>
2025-09-05 18:37:33 -0400

    Apply suggestion from @effigies
    Co-authored-by: Chris Markiewicz <effigies@gmail.com>

--- fmriprep/interfaces/reports.py
+++ fmriprep/interfaces/reports.py
@@ -263,15 +263,10 @@ class FunctionalSummary(SummaryInterface):
 
         if self.inputs.dummy_scans == self.inputs.algo_dummy_scans:
             dummy_scan_msg = f'{self.inputs.dummy_scans} (Confirmed: {self.inputs.algo_dummy_scans} automatically detected)'
-                [
-                    dummy_scan_tmp,
-                    f'(Confirmed: {self.inputs.algo_dummy_scans} automatically detected)',
-                ]
-            ).format(n_dum=self.inputs.dummy_scans)
         # the number of dummy scans was specified by the user and
         # it is not equal to the number detected by the algorithm
         elif self.inputs.dummy_scans is not None:
-            dummy_scan_msg = ' '.join(
+            dummy_scan_msg = f'{self.inputs.dummy_scans} (Warning: {self.inputs.algo_dummy_scans} automatically detected)'
                 [
                     dummy_scan_tmp,
                     f'(Warning: {self.inputs.algo_dummy_scans} automatically detected)',
