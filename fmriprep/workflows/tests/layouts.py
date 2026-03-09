4c9b4984a33447481b0506939c652ec5d0790923
mathiasg <mathiasg@stanford.edu>
2026-03-09 09:19:59 -0400

    rf: generate rather than copy layout components

--- fmriprep/workflows/tests/layouts.py
+++ fmriprep/workflows/tests/layouts.py
@@ -1,130 +1,145 @@
-from copy import deepcopy
+def _make_anat():
+    return [
+        {'run': 1, 'suffix': 'T1w'},
+        {'run': 2, 'suffix': 'T1w'},
+        {'suffix': 'T2w'},
+    ]
 
 
-_anat = [
-    {'run': 1, 'suffix': 'T1w'},
-    {'run': 2, 'suffix': 'T1w'},
-    {'suffix': 'T2w'},
-]
+def _make_func():
+    return [
+        *(
+            {
+                'task': 'rest',
+                'run': i,
+                'suffix': suffix,
+                'metadata': {
+                    'RepetitionTime': 2.0,
+                    'PhaseEncodingDirection': 'j',
+                    'TotalReadoutTime': 0.6,
+                    'EchoTime': 0.03,
+                    'SliceTiming': [0.0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8],
+                },
+            }
+            for suffix in ('bold', 'sbref')
+            for i in range(1, 3)
+        ),
+        *(
+            {
+                'task': 'nback',
+                'echo': i,
+                'suffix': 'bold',
+                'metadata': {
+                    'RepetitionTime': 2.0,
+                    'PhaseEncodingDirection': 'j',
+                    'TotalReadoutTime': 0.6,
+                    'EchoTime': 0.015 * i,
+                    'SliceTiming': [0.0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8],
+                },
+            }
+            for i in range(1, 4)
+        ),
+    ]
 
-_func = [
-    *(
+
+def _make_fmap():
+    return [
+        {'suffix': 'phasediff', 'metadata': {'EchoTime1': 0.005, 'EchoTime2': 0.007}},
+        {'suffix': 'magnitude1', 'metadata': {'EchoTime': 0.005}},
         {
-            'task': 'rest',
-            'run': i,
-            'suffix': suffix,
-            'metadata': {
-                'RepetitionTime': 2.0,
-                'PhaseEncodingDirection': 'j',
-                'TotalReadoutTime': 0.6,
-                'EchoTime': 0.03,
-                'SliceTiming': [0.0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8],
-            },
-        }
-        for suffix in ('bold', 'sbref')
-        for i in range(1, 3)
-    ),
-    *(
-        {
-            'task': 'nback',
-            'echo': i,
-            'suffix': 'bold',
-            'metadata': {
-                'RepetitionTime': 2.0,
-                'PhaseEncodingDirection': 'j',
-                'TotalReadoutTime': 0.6,
-                'EchoTime': 0.015 * i,
-                'SliceTiming': [0.0, 0.2, 0.4, 0.6, 0.8, 1.0, 1.2, 1.4, 1.6, 1.8],
-            },
-        }
-        for i in range(1, 4)
-    ),
-]
-
-_fmap = [
-    {'suffix': 'phasediff', 'metadata': {'EchoTime1': 0.005, 'EchoTime2': 0.007}},
-    {'suffix': 'magnitude1', 'metadata': {'EchoTime': 0.005}},
-    {
-        'suffix': 'epi',
-        'dir': 'PA',
-        'metadata': {'PhaseEncodingDirection': 'j', 'TotalReadoutTime': 0.6},
-    },
-    {
-        'suffix': 'epi',
-        'dir': 'AP',
-        'metadata': {'PhaseEncodingDirection': 'j-', 'TotalReadoutTime': 0.6},
-    },
-]
-
-_no_session = {
-    '01': {
-        'anat': _anat,
-        'func': _func,
-        'fmap': _fmap,
-    },
-}
-
-_single_session = {
-    '01': [
-        {
-            'session': 'pre',
-            'anat': _anat,
-            'func': _func,
-            'fmap': _fmap,
-        }
-    ],
-}
-
-_homogeneous_sessions = {
-    '01': [
-        {
-            'session': 'pre',
-            'anat': _anat,
-            'func': _func,
-            'fmap': _fmap,
+            'suffix': 'epi',
+            'dir': 'PA',
+            'metadata': {'PhaseEncodingDirection': 'j', 'TotalReadoutTime': 0.6},
         },
         {
-            'session': 'post',
-            'anat': _anat,
-            'func': _func,
-            'fmap': _fmap,
+            'suffix': 'epi',
+            'dir': 'AP',
+            'metadata': {'PhaseEncodingDirection': 'j-', 'TotalReadoutTime': 0.6},
         },
     ]
-}
 
-_heterogeneous_sessions = {
-    '01': [
-        {
-            'session': 'anat',
-            'anat': {'suffix': 'T1w'},
-        },
-        {
-            'session': 'fmri',
-            'func': [
-                {
-                    'task': 'rest',
-                    'suffix': 'bold',
-                    'metadata': {
-                        'RepetitionTime': 2.0,
-                        'PhaseEncodingDirection': 'j',
-                        'TotalReadoutTime': 0.6,
-                    },
-                }
-            ],
-        },
-    ]
-}
 
-LAYOUTS = {
-    'no_session': _no_session,
-    'single_session': _single_session,
-    'homogeneous_sessions': _homogeneous_sessions,
-    'heterogeneous_sessions': _heterogeneous_sessions,
+def _make_no_session():
+    return {
+        '01': {
+            'anat': _make_anat(),
+            'func': _make_func(),
+            'fmap': _make_fmap(),
+        },
+    }
+
+
+def _make_single_session():
+    return {
+        '01': [
+            {
+                'session': 'pre',
+                'anat': _make_anat(),
+                'func': _make_func(),
+                'fmap': _make_fmap(),
+            }
+        ],
+    }
+
+
+def _make_homogeneous_sessions():
+    return {
+        '01': [
+            {
+                'session': 'pre',
+                'anat': _make_anat(),
+                'func': _make_func(),
+                'fmap': _make_fmap(),
+            },
+            {
+                'session': 'post',
+                'anat': _make_anat(),
+                'func': _make_func(),
+                'fmap': _make_fmap(),
+            },
+        ]
+    }
+
+
+def _make_heterogeneous_sessions():
+    return {
+        '01': [
+            {
+                'session': 'anat',
+                'anat': {'suffix': 'T1w'},
+            },
+            {
+                'session': 'fmri',
+                'func': [
+                    {
+                        'task': 'rest',
+                        'suffix': 'bold',
+                        'metadata': {
+                            'RepetitionTime': 2.0,
+                            'PhaseEncodingDirection': 'j',
+                            'TotalReadoutTime': 0.6,
+                        },
+                    }
+                ],
+            },
+        ]
+    }
+
+
+_LAYOUT_FACTORIES = {
+    'no_session': _make_no_session,
+    'single_session': _make_single_session,
+    'homogeneous_sessions': _make_homogeneous_sessions,
+    'heterogeneous_sessions': _make_heterogeneous_sessions,
 }
 
 
 def get_layout(layout_id: str):
-    """Get a layout spec by ID."""
-    return deepcopy(LAYOUTS[layout_id])
+    """Get a fresh layout spec by ID."""
+    try:
+        return _LAYOUT_FACTORIES[layout_id]()
+    except KeyError:
+        raise ValueError(f'Unknown layout: {layout_id!r}. Choose from {list(_LAYOUT_FACTORIES)}')
+
 
 __all__ = ['get_layout']
